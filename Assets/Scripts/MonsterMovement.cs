2e3fa7629d0fc70b2a08d86bd458f28beddadbc9
AnuragReddy007 <134464994+AnuragReddy007@users.noreply.github.com>
2025-03-21 15:11:17 -0400

    Basic enemy movement
    player can take dmg but the physical health bar is yet to be built.

---
+++ Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class MosterMovement : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public float moveSpeed;
+    public int patrolDestination;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (patrolDestination == 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[0].position) < 0.1f)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                patrolDestination = 1;
+            }
+        }
+
+        if (patrolDestination == 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, patrolPoints[1].position) < 0.1f)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                patrolDestination = 0;
+            }
+        }
+    }
+}
